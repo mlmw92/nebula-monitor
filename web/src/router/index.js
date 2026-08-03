b5d41125bf18d9a7444a5667afe8adafacc0acf4
Geng Peng <360481991@qq.com>
2026-08-03 17:33:32 +0800

    refactor: 系统设置页移除左侧子菜单，直接展示表单

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -26,14 +26,7 @@ const routes = [
       { path: 'report', name: 'report', component: () => import('../components/ReportView.vue') },
       { path: 'system/upgrade', name: 'system-upgrade', component: () => import('../components/UpgradeView.vue') },
       { path: 'notify', name: 'notify', component: () => import('../components/NotifyView.vue') },
-      {
-        path: 'system/settings',
-        component: () => import('../layouts/SettingsLayout.vue'),
-        redirect: '/system/settings/brand',
-        children: [
-          { path: 'brand', name: 'system-settings', component: () => import('../components/settings/BrandSettingsSubView.vue') },
-        ],
-      },
+      { path: 'system/settings', name: 'system-settings', component: () => import('../components/settings/BrandSettingsSubView.vue') },
     ],
   },
 ]
